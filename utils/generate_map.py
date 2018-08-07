9b78b007618b03bbf94f06d2e3a2e65f3d91619f
Damien Vergnet <darmo117@gmail.com>
2018-08-07 14:49:09 +0200

    fix: generated map file now takes name of input file

--- utils/generate_map.py
+++ utils/generate_map.py
@@ -6,11 +6,12 @@ if len(sys.argv) != 2:
     print(f"Usage: {sys.argv[0]} <python map file>")
     sys.exit(-1)
 
-module = importlib.import_module("utils." + sys.argv[1])
+name = sys.argv[1]
+module = importlib.import_module("utils." + name)
 
 if not hasattr(module, "map_data"):
     print("No 'map_data' variable defined in module!")
     sys.exit(-2)
 
-with open("../data/maps/test.map", "wb") as f:
+with open(f"../data/maps/{name}.map", "wb") as f:
     pickle.dump(module.map_data, f)
